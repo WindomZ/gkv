dbf21cc343a17cd98368e45761dc5ec30d4b99a8
WindomZ <git.windomz@gmail.com>
2018-03-01 21:27:35 +0800

    feat: add buntdb adapter

---
+++ buntdb/buntdb.go
@@ -0,0 +1,83 @@
+package buntdb
+
+import (
+	"path/filepath"
+
+	"github.com/WindomZ/gkv"
+	"github.com/tidwall/buntdb"
+)
+
+// KV is tidwall/buntdb adapter.
+type KV struct {
+	db *buntdb.DB
+}
+
+// Open creates a new buntdb driver by storage file path.
+// paths are storage file paths.
+func Open(paths ...string) gkv.KV {
+	var path string
+	if len(paths) != 0 {
+		path = paths[0]
+	} else {
+		path = filepath.Join(gkv.ProjectDir(), "data", "data.db")
+	}
+	db, err := buntdb.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	return &KV{
+		db: db,
+	}
+}
+
+// DB returns the native DB of the adapter.
+func (kv KV) DB() interface{} {
+	return kv.db
+}
+
+// Close releases all database resources.
+func (kv *KV) Close() error {
+	return kv.db.Close()
+}
+
+// Register initializes a new database if it doesn't already exist.
+func (kv *KV) Register(table []byte) error {
+	return nil
+}
+
+// Put sets the value for a key.
+func (kv *KV) Put(key, value []byte) error {
+	return kv.db.Update(func(tx *buntdb.Tx) error {
+		_, _, err := tx.Set(gkv.Btos(key), gkv.Btos(value), nil)
+		return err
+	})
+}
+
+// Get retrieves the value for a key.
+func (kv *KV) Get(key []byte) (value []byte) {
+	kv.db.View(func(tx *buntdb.Tx) error {
+		val, err := tx.Get(gkv.Btos(key))
+		if err != nil {
+			return err
+		}
+		value = gkv.Stob(val)
+		return nil
+	})
+	return
+}
+
+// Count returns the total number of all the keys.
+func (kv *KV) Count() (i int) {
+	kv.db.View(func(tx *buntdb.Tx) error {
+		err := tx.Ascend("", func(key, value string) bool {
+			i++
+			return true
+		})
+		return err
+	})
+	return
+}
+
+func init() {
+	gkv.Register(Open)
+}
---
+++ buntdb/buntdb_test.go
@@ -0,0 +1,45 @@
+package buntdb
+
+import (
+	"testing"
+
+	"github.com/WindomZ/testify/assert"
+)
+
+var demo *KV
+var (
+	demoTable = []byte("table-表_1 2%3")
+	demoKey   = []byte("key-键_4 5%6")
+	demoValue = []byte("value-值_7 8%9")
+)
+
+func TestOpen(t *testing.T) {
+	db := Open("../data/test-buntdb.db")
+	if v, ok := db.(*KV); ok {
+		demo = v
+	}
+}
+
+func TestDB(t *testing.T) {
+	assert.NotEmpty(t, demo.DB())
+}
+
+func TestRegister(t *testing.T) {
+	assert.NoError(t, demo.Register(demoTable))
+}
+
+func TestPut(t *testing.T) {
+	assert.NoError(t, demo.Put(demoKey, demoValue))
+}
+
+func TestGet(t *testing.T) {
+	assert.Equal(t, demoValue, demo.Get(demoKey))
+}
+
+func TestCount(t *testing.T) {
+	assert.Equal(t, 1, demo.Count())
+}
+
+func TestClose(t *testing.T) {
+	assert.NoError(t, demo.Close())
+}
